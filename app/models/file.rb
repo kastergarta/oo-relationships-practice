810cb945272993c22a4144a4159ab847e8ce778a
Dennis Basyrov <dennis.basyrov@gmail.com>
2019-06-11 16:23:02 -0400

    90 mins

--- app/models/file.rb
+++ app/models/file.rb
@@ -1,1 +1,131 @@
-# create files for your ruby classes in this directory
+class Listing
+
+@@all = []
+attr_accessor :location
+
+def initialize(location)
+    @location = location
+    @@all << self
+end
+
+def self.all
+   @@all
+end
+
+def guests
+  a = []
+  Trip.all.each do |trip|
+  if trip.listing == self
+  a << trip.guest
+end
+end
+return a
+end
+
+def trips
+  Trip.all.select {|trip| trip.listing == self}
+end
+
+def trip_count
+  count = 0
+  Trip.all.each do |trip|
+  if trip.listing == self
+  count += 1
+end
+end
+return count
+end
+
+def self.find_all_by_city(city)
+  self.all.select {|listing| listing.location == city}
+end
+
+
+# def self.most_popular
+#   @@all.sort_by { |listing| listing.trip_count }
+# end
+
+def self.most_popular
+  h = {}
+  @@all.each do |listing|
+  h.merge!(listing.trip_count => listing.location)
+  end
+  return h.sort.reverse[0]
+end
+
+end
+# --------------------------------------------------------------------
+
+class Guest
+@@all = []
+attr_writer :name
+
+def initialize(name)
+  @name = name
+  @@all << self
+end
+
+def self.all
+   @@all
+end
+
+def listings
+  array = []
+  Trip.all.select do |trip|
+    if trip.guest == self
+      array << trip.listing
+    end
+end
+return array
+end
+
+def trips
+  Trip.all.select {|trip| trip.guest == self}
+end
+
+def trip_count
+  array = []
+  Trip.all.select do |trip|
+    if trip.guest == self
+      array << trip.listing
+    end
+end
+return array.length
+end
+
+def self.pro_traveller
+    @@all.select { |guest| guest.trip_count > 1 }
+end
+
+def self.find_all_by_name(name)
+  @@all.select {|guest| guest.name == name}
+end
+
+end
+
+# --------------------------------------------------------------------
+
+class Trip
+@@all = []
+attr_writer :listing, :guest
+
+def initialize(listing, guest)
+  @listing = listing
+  @guest = guest
+  @@all << self
+end
+
+def listing
+  @listing
+end
+
+def guest
+  @guest
+end
+
+def self.all
+   @@all
+end
+
+end
+# --------------------------------------------------------------------
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,72 @@ def reload
   load 'config/environment.rb'
 end
 
+
+listing1 = Listing.new("New York")
+listing2 = Listing.new("Hong Kong")
+listing3 = Listing.new("Berlin")
+
+guest1 = Guest.new("Dennis")
+guest2 = Guest.new("Kevin")
+guest3 = Guest.new("Sean")
+
+trip1 = Trip.new(listing1, guest2)
+trip2 = Trip.new(listing2, guest1)
+trip3 = Trip.new(listing3, guest3)
+trip4 = Trip.new(listing1, guest1)
+trip5 = Trip.new(listing3, guest1)
+trip6 = Trip.new(listing1, guest3)
+
+
 Pry.start
+
+# Listing
+
+# #guests
+# returns an array of all guests who have stayed at a listing!!!!!!!!!
+
+# #trips
+# returns an array of all trips at a listing!!!!!!!!!!!!
+
+# #trip_count!!!!!!!!!!!!
+# returns the number of trips that have been taken to that listing!!!!!!!!!!!
+
+# .find_all_by_city(city)!!!!!!!!!!!!!!
+# takes an argument of a city name (as a string) and returns all the listings for that city
+
+# .most_popular
+# finds the listing that has had the most trips!!!!!!!!!
+
+
+
+
+
+# Guest
+
+# #listings
+# returns an array of all listings a guest has stayed at !!!!!!!!!!!!!!!!
+
+# #trips
+# returns an array of all trips a guest has made !!!!!!!!!!!!!!!!!!
+
+# #trip_count
+# returns the number of trips a guest has taken!!!!!!!!!!!!!!!
+
+# .all
+# returns an array of all guests!!!!!!!!!!!!!!!!
+
+# .pro_traveller
+# returns an array of all guests who have made over 1 trip !!!!!!!!
+
+# .find_all_by_name(name)
+# takes an argument of a name (as a string), returns the all guests with that name !!!!!!!!!!
+
+
+
+# Trip
+
+# #listing
+# returns the listing object for the trip!!!!!!!!!!!!
+
+# #guest
+# returns the guest object for the trip!!!!!!!!!!!!!
